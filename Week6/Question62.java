6593edcb0808e238339b6465e42d9eed1df77d3a
Tosmim <tosmimforidmehtab@gmail.com>
2023-03-05 11:27:53 +0530

    Week 6

---
+++ Week6/Question62.java
@@ -0,0 +1,16 @@
+public class Question62 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.print(Thread.currentThread().getName() + " has ended.");
+    }
+
+    public static void main(String[] args) {
+        Question62 th = new Question62();
+        Thread t1 = new Thread(th);
+        t1.setName("Main Thread");
+        t1.start();
+        System.out.println("Welcome to Java Week 6 New Question.");
+        // t1.setName("Main Thread");
+    }
+}
