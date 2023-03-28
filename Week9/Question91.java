4e0d9fe159d3a085782c21476c56ca261c311182
Tosmim <tosmimforidmehtab@gmail.com>
2023-03-28 17:23:13 +0530

    Week 9

---
+++ Week9/Question91.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Question91 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int i = 0, j = 0;
+        double output = 0;
+        // Split the input string into character array
+        char seq[] = input.toCharArray();
+        /*
+         * Use some method to separate the two operands
+         * and then perform the required operation.
+         */
+        for (int a = 0; a < seq.length; a++) {
+            if (seq[a] == '+') {
+                i = Integer.parseInt(input.substring(0, a));
+                j = Integer.parseInt(input.substring(a + 1, seq.length));
+                output = (double) i + j;
+            } else if (seq[a] == '-') {
+                i = Integer.parseInt(input.substring(0, a));
+                j = Integer.parseInt(input.substring(a + 1, seq.length));
+                output = (double) i - j;
+            } else if (seq[a] == '/') {
+                i = Integer.parseInt(input.substring(0, a));
+                j = Integer.parseInt(input.substring(a + 1, seq.length));
+                output = (double) i / j;
+            } else if (seq[a] == '*') {
+                i = Integer.parseInt(input.substring(0, a));
+                j = Integer.parseInt(input.substring(a + 1, seq.length));
+                output = (double) i * j;
+            }
+        }
+        // Print the output as stated in the question
+        System.out.print(input + " = " + Math.round(output));
+    }
+}
